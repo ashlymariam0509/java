d8525a61a9c5c2db561344fd29decf74d12b91ed
ashlymariam0509 <ashlysank2006@gmail.com>
2025-07-20 18:36:45 +0530

    Add files via upload

---
+++ Largestnumber.java
@@ -0,0 +1,33 @@
+package pro;
+import java.util.Scanner;
+public class Largestnumber
+{
+ public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter  the first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter  the third number: ");
+        int num3 = sc.nextInt();
+
+        int largest;
+
+        // Check which is largest
+        if(num1 >= num2 && num1 >= num3) {
+            largest = num1;
+        } else if(num2 >= num1 && num2 >= num3) {
+            largest = num2;
+        } else {
+            largest = num3;
+        }
+
+        System.out.println("The largest number is: " + largest);
+
+        sc.close();
+ }
+} 
+
